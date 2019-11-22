e55ca0c7686a9b84076363f2c6f3de0cbc6ba781
Dorociak <marcin.dorociak@sage.com>
2019-11-22 09:57:29 +0100

    work in progress ocr

--- ocrAnalysisOfReport/scroll.py
+++ ocrAnalysisOfReport/scroll.py
@@ -6,29 +6,36 @@ import cv2
 
 time.sleep(5)
 i = 0
-myScreenshot = pyautogui.screenshot(region=(0, 197, 180, 50))
-myScreenshot.save(r'filename0_' + str(i) + '.png')
-myScreenshot = pyautogui.screenshot(region=(190, 197, 1540, 797))
-myScreenshot.save(r'filename1_' + str(i) + '.png')
 old_cur_pos = 0
 
-while i < 20:
+def change_colors(img)
+    img[np.where((im2 == [0, 255, 0]).all(axis=2))] = [0, 255, 255]
+
+    return img
+def scroll_horizontally (direction):
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+    pyautogui.press(direction)
+
+while not pyautogui.locateOnScreen('bottom_end.png', region=(1700, 900, 60, 100)):
         l = 0
-    # while l < 24:
-    #     l += 1
-    #     if l == 24:
-    #         i += 1
-    #         myScreenshot = pyautogui.screenshot(region=(0, 197, 180, 50))
-    #         myScreenshot.save(r'filename0_' + str(i) + '.png')
-    #         myScreenshot = pyautogui.screenshot(region=(190, 197, 1540, 797))
-    #         myScreenshot.save(r'filename1_' + str(i) + '.png')
-    #         break
-        cur_possition = pyautogui.locateOnScreen('break_line.png', region=(230, 800,21, 210))
+        height2 = 0
+        top2 = 0
+        cur_possition = pyautogui.locateOnScreen('break_line.png', region=(230, 900,21, 100))
         if cur_possition:
-            print(cur_possition)
             cur_possition = list(pyautogui.locateAllOnScreen('break_line.png', region=(230, 197, 21, 797)))
             list_length = len(cur_possition)
-            print(cur_possition)
             if len(cur_possition) == 1:
                 top = 197
                 height = cur_possition[0][1] - top
@@ -38,24 +45,52 @@ while i < 20:
             else:
                 top = cur_possition[list_length-2][1]
                 height = cur_possition[list_length-1][1] - top
-            print (top)
-            print (height)
+                if cur_possition[list_length-2][1] > 900
+                    top2 = cur_possition[list_length-3][1]
+                    height2 = cur_possition[list_length-2][1] - top2
+
             i += 1
-            myScreenshot = pyautogui.screenshot(region=(0, top, 180, 50))
-            myScreenshot = cv2.cvtColor(np.array(myScreenshot), cv2.COLOR_RGB2BGR)
+
+            if top2 > 0
+                myScreenshot = pyautogui.screenshot(region=(0, top2, 150, 50))
+                myScreenshot = cv2.cvtColor(np.array(myScreenshot), cv2.COLOR_RGB2BGR)
+                cv2.imwrite('filename0_' + str(i) + '.png', myScreenshot)
+
+                myScreenshot1 = pyautogui.screenshot(region=(180, top2, 1555, height2))
+                myScreenshot1 = cv2.cvtColor(np.array(myScreenshot1), cv2.COLOR_RGB2BGR)
+
+            scroll_horizontally("right")
+            if top2 > 0
+                myScreenshot2 = pyautogui.screenshot(
+                    region=(180, top2, 1555, height2))
+                myScreenshot2 = cv2.cvtColor(np.array(myScreenshot2), cv2.COLOR_RGB2BGR)
+
+                myScreenshot = np.concatenate(
+                    (myScreenshot1, myScreenshot2), axis=1)
+                cv2.imwrite('filename2_' + str(i) + '.png', myScreenshot)
+                i += 1
+
+            myScreenshot = pyautogui.screenshot(region=(0, top, 150, 50))
+            myScreenshot = cv2.cvtColor(
+                np.array(myScreenshot), cv2.COLOR_RGB2BGR)
             cv2.imwrite('filename0_' + str(i) + '.png', myScreenshot)
 
-            myScreenshot = pyautogui.screenshot(region=(190, top, 1540, height))
-            myScreenshot = cv2.cvtColor(np.array(myScreenshot), cv2.COLOR_RGB2BGR)
-            cv2.imwrite('filename1_' + str(i) + '.png', myScreenshot)
-            pyautogui.scroll(-100)
-            pyautogui.scroll(-100)
-            pyautogui.scroll(-100)
-            pyautogui.scroll(-100)
-            pyautogui.scroll(-100)
-            pyautogui.scroll(-100)
+            myScreenshot2 = pyautogui.screenshot(
+                region=(180, top, 1555, height))
+            myScreenshot2 = cv2.cvtColor(
+                np.array(myScreenshot2), cv2.COLOR_RGB2BGR)
 
-        pyautogui.scroll(-100)
+            scroll_horizontally("left")
+
+            myScreenshot1 = pyautogui.screenshot(
+                region=(180, top, 1555, height))
+            myScreenshot1 = cv2.cvtColor(
+                np.array(myScreenshot1), cv2.COLOR_RGB2BGR)
+            myScreenshot = np.concatenate(
+                (myScreenshot1, myScreenshot2), axis=1)
+            cv2.imwrite('filename2_' + str(i) + '.png', myScreenshot)
+            
+        pyautogui.press('down')
 
 
 
